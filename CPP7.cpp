770dc1e02bc455178987ec2fab2127c7e526b004
Suritra Bandyopadhyay <31152879+SurisGithub@users.noreply.github.com>
2017-10-27 23:54:28 +0530

    Update CPP7.cpp

--- CPP7.cpp
+++ CPP7.cpp
@@ -16,7 +16,46 @@ int Strlen(char ch[])
    for(i=0;ch[i]!='\0';i++);
    return i;
 }
-
+void Strchr(char str[])
+{
+   int flag=0,i;
+   char ch;
+   cout<<"\nEnter the character to be searched for: ";
+   cin>>ch;
+   for(i=0;str[i]!='\0';i++)
+   {
+      if(ch==str[i])
+      {  flag=1;
+         break;
+      }
+   }
+   if(flag==1)
+     cout<<"\nCharacter found at position: "<<(i+1);
+   else
+     cout<<"\nCharacter was not found ! ! ! !"; 
+}
+void Strstr(char str[],char substr[])
+{
+    /* write your code here */
+    int i = 0, j = 0;
+   
+    while ((str[j] != '\0')||(substr[i] != '\0')) 
+    {
+        if (substr[i] != str[j])
+        {
+            j++;
+            i = 0;
+        }
+        else {
+            i++;
+            j++;
+        }
+    }
+    if (substr[i] == '\0')
+        cout<<"\nSubstring found at "<<j;
+    else
+        cout<<"\nSubstring not found ! ! !";
+}
 void Strcat(char st1[],char st2[])
 {
    int x=0;
@@ -58,7 +97,11 @@ int main()
    cout<<"Both the strings are unequal";
   cout<<"\nThe length of String1 and String2 respectively is: "<<Strlen(str1)<<" and "<<Strlen(str2);
   Strcat(str1,str2);
-  
+  Strchr(str1);
+  char substr[20];
+   cout<<"\nEnter the Subtring to be searched: ";
+   cin>>substr;
+   Strstr(str1,substr);
   return 0;
 }
   
